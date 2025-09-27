b8de7f9a121a1347cf1283b6ec801b59eeb0c177
sreegopalsaha <sreegopal0101@gmail.com>
2025-09-27 10:43:13 +0530

    add data.ts

---
+++ data/data.ts
@@ -0,0 +1,55 @@
+import { Project, SocialLinks } from "@/types";
+
+export const about: string[] = [
+  "I'm Sree Gopal Saha, a MERN Stack Developer from West Bengal, India.",
+  "Experienced in building full-stack web apps.",
+  "Ex-Intern at Blogrator Web Services Pvt. Ltd.",
+  "Track Winner at Binary’25, a 36-hour Hackathon.",
+  "Connect with me on LinkedIn.",
+];
+
+export const projects: Project[] = [
+  { name: "Ryve", href: "https://www.ryve.xyz" },
+  { name: "Wraptalk", href: "https://www.wraptalk.xyz" },
+  { name: "more", href: "https://github.com/sreegopalsaha" },
+];
+
+export const frontend: string[] = [
+  "React.js",
+  "Next.js",
+  "TypeScript",
+  "Tailwind CSS",
+  "Shadcn/UI",
+  "Responsive Design",
+  "Figma",
+];
+
+export const backend: string[] = [
+  "Node.js",
+  "Express.js",
+  "TypeScript",
+  "MongoDB",
+  "MySQL",
+  "Prisma",
+  "Drizzle",
+  "RESTful APIs",
+  "JWT",
+  "Socket.io",
+];
+
+export const tools: string[] = [
+  "Gemini API",
+  "OpenAI API",
+  "Hugging Face",
+  "Git",
+  "GitHub",
+  "Vercel",
+  "Netlify",
+  "Postman",
+];
+
+export const socialLinks: SocialLinks = {
+  email: "mailto:sreegopal0101@gmail.com",
+  linkedin: "https://linkedin.com/in/sreegopalsaha",
+  github: "https://github.com/sreegopalsaha",
+};
